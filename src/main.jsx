ebf77a2fbe239b84dca00529930820f089dc0349
SaiSupratisha <davalasaisupratisha@thestackly.com>
2026-06-30 11:53:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
  
 import App from "./App";
  
-import WishlistProvider from "./context/WishlistContext";
+import WishlistProvider from "./Context/WishlistContext";
  
 import "./index.css";
  
