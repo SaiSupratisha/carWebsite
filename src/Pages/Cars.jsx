edee324de011ce1be54193a6915f507e80e782fe
SaiSupratisha <davalasaisupratisha@thestackly.com>
2026-06-30 12:13:41 +0530

    Fix import casing for component files in Cars.jsx
    Updated import paths to use consistent casing for components.

--- src/Pages/Cars.jsx
+++ src/Pages/Cars.jsx
@@ -1,9 +1,9 @@
 import { useSearchParams } from "react-router-dom";
  
-import CarCard from "../components/CarCard";
-import Loader from "../components/Loader";
-import SearchBar from "../components/SearchBar";
-import Pagination from "../components/Pagination";
+import CarCard from "../Components/CarCard";
+import Loader from "../Components/Loader";
+import SearchBar from "../Components/SearchBar";
+import Pagination from "../Components/Pagination";
 import useFetchCars from "../hooks/useFetchCars";
  
 import "../styles/cars.css";
